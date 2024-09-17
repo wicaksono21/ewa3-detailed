7494f3816304149993d0b40dd6927eb423d1ed4a
wicaksono21 <131900912+wicaksono21@users.noreply.github.com>
2024-09-17 17:40:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def handle_chat(prompt):
     
     # Simulate AI response
     response = OpenAI(api_key=st.secrets["default"]["OPENAI_API_KEY"]).chat.completions.create(
-        model="gpt-4o-2024-08-06",
+        model="gpt-4o-mini",
         messages=st.session_state["messages"],
         temperature=1,
         presence_penalty=0.5,   # Penalizes repeating ideas
